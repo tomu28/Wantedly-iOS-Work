2c4f7e6d9af0ce6c7052c51fbbb0079ea51b5dac
tomu28 <b1700662@planet.kanazawa-it.ac.jp>
2019-02-23 18:50:32 +0900

    create a new Xcode project

---
+++ Wantedly-iOS-Work/Wantedly-iOS-Work/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Wantedly-iOS-Work
+//
+//  Created by 小幡 十矛 on 2019/02/23.
+//  Copyright © 2019 Tomu Obata. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
