40ff4d86671ef1846239f9e2e9b4587b24acad29
tomu28 <b1700662@planet.kanazawa-it.ac.jp>
2019-02-24 23:53:52 +0900

    WantedlyAPIから１画面目の表示に必要なテキストデータを取得

--- Wantedly-iOS-Work/Wantedly-iOS-Work/TopViewController.swift
+++ Wantedly-iOS-Work/Wantedly-iOS-Work/TopViewController.swift
@@ -7,14 +7,44 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
 class TopViewController: UIViewController {
 
+    var APIDataList: [[String: String?]] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        getAPIData()
+        
     }
-
-
+    
+    func getAPIData() {
+        
+        let _ = Alamofire.request("https://www.wantedly.com/api/v1/projects?q=swift&page=1").responseJSON {
+            response in guard let object = response.result.value else {
+                return
+            }
+            let json = JSON(object)
+            
+            // JSONのdataオブジェクトを取り出す
+            let dataObject = json["data"]
+            
+            dataObject.forEach {(key, dataObject) in
+                let APIData: [String: String?] = [
+                    "title": dataObject["title"].string,
+                    "looking_for": dataObject["looking_for"].string,
+                    "companyName": dataObject["company"]["name"].string
+                    ]
+                 self.APIDataList.append(APIData)
+            }
+             print(self.APIDataList)
+            
+        }
+        
+    }
+    
 }
-
