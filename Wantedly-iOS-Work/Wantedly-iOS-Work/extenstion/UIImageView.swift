6e0742e0e1072280bd27cf2730df923b27184333
tomu28 <b1700662@planet.kanazawa-it.ac.jp>
2019-02-26 18:28:41 +0900

    extension:UIImageViewの記述

--- Wantedly-iOS-Work/Wantedly-iOS-Work/extenstion/UIImageView.swift
+++ Wantedly-iOS-Work/Wantedly-iOS-Work/extenstion/UIImageView.swift
@@ -6,4 +6,32 @@
 //  Copyright © 2019 Tomu Obata. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+extension UIImageView {
+    func loadImageAsynchronously(url: URL?, defaultUIImage: UIImage? = nil) -> Void {
+        
+        if url == nil {
+            self.image = defaultUIImage
+            return
+        }
+        
+        DispatchQueue.global().async {
+            do {
+                let imageData: Data? = try Data(contentsOf: url!)
+                DispatchQueue.main.async {
+                    if let data = imageData {
+                        self.image = UIImage(data: data)
+                    } else {
+                        self.image = defaultUIImage
+                    }
+                }
+            }
+            catch {
+                DispatchQueue.main.async {
+                    self.image = defaultUIImage
+                }
+            }
+        }
+    }
+}
