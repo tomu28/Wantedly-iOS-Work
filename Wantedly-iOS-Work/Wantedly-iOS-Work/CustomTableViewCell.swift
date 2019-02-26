7eb7b6049044a04e87433b43cfef081c1d6da39e
tomu28 <b1700662@planet.kanazawa-it.ac.jp>
2019-02-26 17:27:03 +0900

    スタイル修正

---
+++ Wantedly-iOS-Work/Wantedly-iOS-Work/CustomTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CuntomTableViewCell.swift
+//  Wantedly-iOS-Work
+//
+//  Created by 小幡 十矛 on 2019/02/26.
+//  Copyright © 2019 Tomu Obata. All rights reserved.
+//
+
+import UIKit
+
+class CuntomTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
