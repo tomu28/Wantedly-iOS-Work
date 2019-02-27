f1a440483d9c0d2c7b4842af3b3a60a54b7f953c
tomu28 <b1700662@planet.kanazawa-it.ac.jp>
2019-02-27 14:12:17 +0900

    NavigationControllerを追加

--- Wantedly-iOS-Work/Wantedly-iOS-Work/TopViewController.swift
+++ Wantedly-iOS-Work/Wantedly-iOS-Work/TopViewController.swift
@@ -27,6 +27,8 @@ class TopViewController: UIViewController, UITableViewDelegate, UITableViewDataS
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        navigationController?.setNavigationBarHidden(true, animated: false)
+        
         getAPIData()
         print("viewDidLoad")
         print(APIDataCount)
